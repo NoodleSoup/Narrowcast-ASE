2ba1fd0d18a950cef2bcba31ad56fc65135d7820
Daan <eekhof.daan@gmail.com>
2020-06-02 17:44:23 +0200

    [ADD] Added login page to the page router

--- DockerDB-Projects/Narrowcast-Frontend/src/router/index.js
+++ DockerDB-Projects/Narrowcast-Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'dashboard',
     component: Dashboard
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   },
   {
     path: '/search',
