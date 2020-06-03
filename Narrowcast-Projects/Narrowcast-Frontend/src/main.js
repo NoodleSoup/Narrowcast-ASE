330d8b6d219376f059f1f044feef24d978144ef2
Daan <eekhof.daan@gmail.com>
2020-06-03 18:26:59 +0200

    Make OAuth globally available
    - Can be changed to API only, will be done later

--- Narrowcast-Projects/Narrowcast-Frontend/src/main.js
+++ Narrowcast-Projects/Narrowcast-Frontend/src/main.js
@@ -5,6 +5,15 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+var OAuth = require('@zalando/oauth2-client-js');
+var narrowcast = new OAuth.Provider({
+  id: 'narrowcast',   // required
+  authorization_url: 'https://github.com/login/oauth/authorize' // required
+});
+
+Vue.prototype.$narrowcast = narrowcast
+Vue.prototype.$OAuth = OAuth
+
 new Vue({
   router,
   store,
