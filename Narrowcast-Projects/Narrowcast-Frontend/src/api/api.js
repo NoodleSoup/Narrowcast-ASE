02d11e526405411e5ece4bb838a0d1b972af7ac3
Daan <eekhof.daan@gmail.com>
2020-06-04 20:43:22 +0200

    Added CORS error workaround and api call to get user account info from GitHub

--- Narrowcast-Projects/Narrowcast-Frontend/src/api/api.js
+++ Narrowcast-Projects/Narrowcast-Frontend/src/api/api.js
@@ -24,15 +24,32 @@ export default {
     },
     getAccessToken(code){
         return new Promise((resolve, reject) => {
-            axios.post('https://github.com/login/oauth/access_token', {
+            axios.post('https://cors-anywhere.herokuapp.com/'+'https://github.com/login/oauth/access_token', {
                 client_id: '1898318385df9c514c57',
                 client_secret: '0ddfa337f38c62f58200ceda964521fb54a4435a',
-                code: code})
+                code: code,
+                credentials: 'omit',
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'POST'}})
                 .then(data => {
                     resolve(data.data);
                 }).catch(error => {
                     reject(error);
                 });
         });
+    },
+    getUserData(){
+        return new Promise((resolve, reject) => {
+            axios.get('https://api.github.com/user', {
+                headers:{
+                    'Authorization': `token ${localStorage.token}`
+                }
+            }).then(data => {
+                resolve(data.data);
+            }).catch(error => {
+                reject(error);
+            })
+        })
     }
 }
