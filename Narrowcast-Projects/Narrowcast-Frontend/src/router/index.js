ea7d86e15e9fae4dad1122e040009adac3b9da26
Daan <eekhof.daan@gmail.com>
2020-06-04 20:43:53 +0200

    Added redirecting from pages not logged in will always redirect to login page otherwise always home page

--- Narrowcast-Projects/Narrowcast-Frontend/src/router/index.js
+++ Narrowcast-Projects/Narrowcast-Frontend/src/router/index.js
@@ -9,23 +9,55 @@ const routes = [
   {
     path: '/',
     name: 'login',
-    component: Login
+    component: Login,
+    meta: {
+      hideForAuth: true
+    }
   },
   {
     path: '/home',
     name: 'home',
-    component: Dashboard
+    component: Dashboard,
+    meta:{
+      requiresAuth: true
+    }
   },
   {
+    // Debugging mainly, might not be in the final release
     path: '/search',
     name: 'search',
-    component: () => import('../views/Search.vue')
+    component: () => import('../views/Search.vue'),
+    meta: {
+      requiresAuth: true
+    }
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)){
+    if (!localStorage.token && !to.query['code'] && !to.fullPath.includes('github')){
+      next({name: 'login'})
+    }else{
+      next();
+    }
+  }else{
+    next();
+  }
+
+  if (to.matched.some(record => record.meta.hideForAuth)) {
+    if (localStorage.token) {
+      next({ name: 'home' })
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 })
 
 export default router
