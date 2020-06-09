3338cd908635ab34cc110d29666a148580d3d739
Daan <eekhof.daan@gmail.com>
2020-06-09 19:07:18 +0200

    Added initial presence page

---
+++ Narrowcast-Projects/Narrowcast-Frontend/src/views/Presence.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="dashboard background-page">
+    <header>
+      <h1 v-if="selectedContainer !== null" v-text="returnDisplayName(selectedContainer)" />
+      <h1 class="test" v-text="returnAccountName()" />
+      <button class="button button-main test" type="submit" v-on:click="printUserData($event)">Test</button>
+      <button class="button button-main log-out" type="submit" v-on:click="logOut($event)">{{ $t('main.logout') }}</button>
+      <button class="button button-main account-set" type="submit" v-on:click="getAccountSettings()">{{ $t('main.accountSettings') }}</button>
+    </header>
+    <aside>
+      <div class="title">
+        <span>{{pageTitle}}</span>
+      </div>
+      <input v-model="filterTerm" type="text" v-bind:placeholder="$t('dashboard.filterCourses')" :disabled="courses === null"/>
+      <ul>
+        <li v-for="(courses, index) in filteredItems" :key="index">
+          <a href="#" @click="selectContainer(courses.courseName, $event)" :class="[courses.courseName === selectedContainer ? 'active' : '']">{{returnDisplayName(courses.courseName)}}</a>
+        </li>
+      </ul>
+    </aside>
+    <main>
+      <vue-dropdown v-if="selectedContainer !== null" :config="configDropdown" @setSelectedOption="setNewSelectedOption($event)"></vue-dropdown>
+      <input v-if="selectedContainer !== null" v-model="filterDataTerm" type="text" v-bind:placeholder="$t('dashboard.filterTerm')" :disabled="selectedContainer === null" v-on:input="selectData(filterDataTerm, $event)" />
+      <List :name="selectedContainer" :filter="filterData" :filterby="filterByData" />
+    </main>
+  </div>
+</template>
+
+<script>
+import VueDropdown from 'vue-dynamic-dropdown'
+import { Api, Login } from '@/api/index.js'
+import { List } from '@/components/common'
+
+export default {
+  name: 'dashboard',
+  components: {
+    VueDropdown,
+    List
+  },
+  data() {
+    return {
+      pageTitle: '',
+      courses: null,
+      filterTerm: '',
+      filterDataTerm: '', // initialize custom filter input
+      filterByData: 'teacherFirst', // set default filter
+      filterData: '', // used to store the custom filter input
+      filteredItems: null,
+      selectedContainer: null,
+      token: '',
+
+      configDropdown: {
+        options: [
+          {text: this.$t('dashboard.dropdown.firstname'), value: 'teacherFirst'},{text: this.$t('dashboard.dropdown.lastname'), value: 'teacherLast'},{text: this.$t('dashboard.dropdown.email'), value: 'eMail'},
+          {text: this.$t('dashboard.dropdown.phonenumber'), value: 'phoneNumber'},{text: this.$t('dashboard.dropdown.location'), value: 'classLocation'}
+        ],
+        prefix: this.$t('dashboard.dropdown.prefix'),
+        placeholder: this.$t('dashboard.dropdown.placeholder')
+        // setting hover text color and text background needs to be set in the vue-dynamic-dropdown module
+      }
+    }
+  },
+  watch: {
+    filterTerm: function() {
+      this.filteredCourses();
+    }
+  },
+  methods: {
+    filteredCourses() {
+      this.filteredItems = this.courses.filter(course => {
+        return course.courseName.toLowerCase().indexOf(this.filterTerm.toLowerCase()) > -1
+      });
+    },
+    selectContainer(name, event) {
+      if (event) event.preventDefault();
+      this.selectedContainer = name;
+    },
+    selectData(filter, event){
+      if(event) event.preventDefault();
+      this.filterData = filter;
+    },
+    setNewSelectedOption(selectedOption) {
+      this.configDropdown.placeholder = selectedOption.text;
+      this.filterByData = selectedOption.value;
+    },
+    returnDisplayName(name) {
+      return name.split('.').join(' ');
+    },
+    getCodeFromUri(){
+      let uri = window.location.search.substring(1);
+      let params = new URLSearchParams(uri);
+      return params.get("code");
+    },
+    printUserData(){
+      Api.getUserData().then(data => {
+        // eslint-disable-next-line
+        console.log(data);
+        alert(data['name']);
+      })
+    },
+    logOut(){
+      localStorage.removeItem('token');
+      this.$router.push('/');
+    },
+    returnAccountName(){
+      Api.getUserData().then(data => {
+        return data['login'];
+      })
+    },
+    getTokenExpiry(){
+      const itemStr = localStorage.getItem('token')
+      // if the item doesn't exist, return null
+      if (!itemStr) {
+        return null
+      }
+      const item = JSON.parse(itemStr)
+      const now = new Date()
+      // compare the expiry time of the item with the current time
+      if (now.getTime() > item.expiry) {
+        // If the item is expired, delete the item from storage
+        // and return null
+        localStorage.removeItem('token')
+        this.$router.push('/');
+      }
+    },
+    getAccountSettings(){
+      this.$router.push('/account')
+    }
+  },
+  mounted() {
+    Api.getCourses().then(data => {
+        this.courses = data;
+        this.filteredCourses();
+    })
+    this.getTokenExpiry();
+
+  },
+  created(){
+    if (localStorage.getItem('token')){
+      this.token = localStorage.getItem('token');
+      return
+    }
+    else{
+      Login.getAccessToken(this.getCodeFromUri()).then(data => {
+        let split_data = data.split('&');
+        const date = new Date()
+        const access_token = {
+          value: split_data[0].split('=')[1],
+          expiry: date.getTime() + 300000 // set token expiry to 5 minutes
+        }
+        let scopes = split_data[1].split('=')[1];
+        let token_type = split_data[2].split('=')[1];
+        // eslint-disable-next-line
+        console.log(`Type: ${token_type} Scopes: ${scopes}`);
+        localStorage.setItem('token', JSON.stringify(access_token));
+        this.$router.replace('/home');
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "@/scss/Dashboard.scss";
+  @import "@/scss/Main.scss";
+</style>
